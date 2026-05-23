5ccb81f8b6f0754341678350982f19469597724d
mca-programmer <freelancermca52@gmail.com>
2026-05-23 19:11:08 +0600

    added routes

---
+++ src/app/routes/index.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { AuthRoutes } from "../modules/auth/auth.route";
+import { IssueRoutes } from "../modules/issue/issue.route";
+
+const router = Router();
+
+/**
+ * Health Check (optional but good for marks)
+ */
+router.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "DevPulse API is running",
+  });
+});
+
+/**
+ * Auth Module Routes
+ */
+router.use("/auth", AuthRoutes);
+
+/**
+ * Issue Module Routes
+ */
+router.use("/issues", IssueRoutes);
+
+export default router;
